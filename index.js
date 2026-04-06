a06bab7fb74a578b22d0a71d1ad156487ae809b7
vale2402 <valivalentina293@gmail.com>
2026-04-07 00:04:29 +0300

    Etapa 4

--- index.js
+++ index.js
@@ -19,21 +19,34 @@ console.log("Folder index.js", __dirname);
 console.log("Folder curent (de lucru)", process.cwd());
 console.log("Cale fisier", __filename);
 
+let vect_foldere=[ "temp", "logs", "backup", "fisiere_uploadate" ]
+for (let folder of vect_foldere){
+    let caleFolder=path.join(__dirname, folder);
+    if (!fs.existsSync(caleFolder)) {
+        fs.mkdirSync(path.join(caleFolder), {recursive:true});   
+    }
+}
+
 // app.get("/resurse/css/general.css", function(req, res){
 //     res.sendFile(path.join(__dirname, "/resurse/css/general.css"));
 // });
 
 app.use("/resurse", express.static(path.join(__dirname, "resurse")));
 
-app.get("/", function(req, res){
-    //res.sendFile(path.join(__dirname, "index.html"));
-    res.render("pagini/index")
-    // ,{
-    //     ip: req.id,
-    //     imagini: obGlobal.obImagini.imagini
-    // };
+app.get("/favicon.ico", function(req, res){
+    res.sendFile(path.join(__dirname, "resurse/imagini/favicon/favicon.ico"));
 });
-//deoarece pe linux ar trebui backslash
+
+// app.get("/", function(req, res){
+//     //res.sendFile(
+// __dirname, "index.html"));
+//     res.render("pagini/index")
+//     // ,{
+//     //     ip: req.ip,
+//     //     imagini: obGlobal.obImagini.imagini
+//     // };
+// });
+// //deoarece pe linux ar trebui backslash
 
 
 // app.get("/cale/:a/:b", function(req, res){
@@ -41,15 +54,16 @@ app.get("/", function(req, res){
 //     res.send("Raspuns pentru <b style='color:red;'>ruta</b> /cale");
 // })
 
-app.get("/:a/:b", function(req, res){
-    console.log(parseInt(req.params.a)+parseInt(req.params.b));
-    res.send("Raspuns pentru <b style='color:red;'>ruta</b> /cale");
-})
+// app.get("/:a/:b", function(req, res){
+//     console.log(parseInt(req.params.a)+parseInt(req.params.b));
+//     res.send("Raspuns pentru <b style='color:red;'>ruta</b> /cale");
+// })
 
-app.get("/cale", function(req, res){
-    console.log("S-a accesat ruta /cale");
-    res.send("Raspuns pentru <b style='color:red;'>ruta</b> /cale");
-})
+
+// app.get("/cale", function(req, res){
+//     console.log("S-a accesat ruta /cale");
+//     res.send("Raspuns pentru <b style='color:red;'>ruta</b> /cale");
+// })
 
 
 
@@ -62,25 +76,26 @@ app.get("/cale2", function(req, res){
 
 function initErori(){
     let continut = fs.readFileSync(path.join(__dirname,"resurse/json/erori.json")).toString("utf-8");
-    let erori=obGlobal.obErori=JSON.parse(continut)
-    let err_default=erori.eroare_default
-    err_default.imagine=path.join(erori.cale_baza, err_default.imagine)
+    let erori=obGlobal.obErori=JSON.parse(continut);
+    let err_default=erori.eroare_default;
+    err_default.imagine=path.join(erori.cale_baza, err_default.imagine);
     for (let eroare of erori.info_erori){
-        eroare.imagine=path.join(erori.cale_baza, eroare.imagine)
+        eroare.imagine=path.join(erori.cale_baza, eroare.imagine);
     }
 
 }
 initErori()
 
 function afisareEroare(res,identificator,titlu,text,imagine){
-    let eroare = obGlobal.obErori.info_erori.find((elem) => elem.identificator==identificator)
+    let eroare = obGlobal.obErori.info_erori.find((elem) => elem.identificator==identificator);
     if(eroare?.status) //?. verifica si returneaza proprietatea
         res.status(eroare.identificator);
-    let errDefault=obGlobal.obErori.eroare_default
+    let errDefault=obGlobal.obErori.eroare_default;
      res.render("pagini/eroare",{
         imagine: imagine || eroare?.imagine || errDefault.imagine,
         titlu: titlu || eroare?.titlu || errDefault.titlu,
         text: text || eroare?.text || errDefault.text,
+        ip: res.req.ip
     });
 }
 
@@ -90,12 +105,12 @@ app.get("/eroare", function(req,res){
 });
 
 app.get(["/", "/index","/home"], function(req, res){
-    res.render("pagini/index",{ip:req.id})
+    res.render("pagini/index",{ip:req.ip})
 });
 
 app.get("/*pagina",function(req,res){
     console.log("cale pagina",req.url);
-    if(req.url.startswith("/resurse") && path.extname(req.url)==""){
+    if(req.url.startsWith ("/resurse") && path.extname(req.url)==""){
         afisareEroare(res,403);
         return;
     }
